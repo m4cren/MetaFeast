fba7dfb56aaf4e7c540ee162a106a726fc8c77f4
m4cren <renrenofficial@gmail.com>
2025-03-09 14:53:02 +0800

    ready for development

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import AdminConfirmation from "./routes/AdminConfirmation";
-import CostumerAuthentication from "./routes/CostumerAuthentication";
-
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/admin" element={<AdminConfirmation />} />
-                <Route path="/" element={<CostumerAuthentication />} />
-            </Routes>
-        </Router>
-    );
+    return <div></div>;
 };
 
 export default App;
